0663dca95d1616a08630828cf9ebcb8e1f2fb976
takealook97 <takealook97@naver.com>
2024-08-31 08:35:38 +0900

    chore: add redis dependency

--- src/main/java/site/udtk/sentenceapi/SentenceApiApplication.java
+++ src/main/java/site/udtk/sentenceapi/SentenceApiApplication.java
@@ -2,8 +2,10 @@ package site.udtk.sentenceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SentenceApiApplication {
 
 	public static void main(String[] args) {
