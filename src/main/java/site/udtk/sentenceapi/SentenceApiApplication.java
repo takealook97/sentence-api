924fddc0fcd299245fe9e34c9bcfee16d8a745f4
takealook97 <takealook97@naver.com>
2024-08-31 07:54:14 +0900

    chore: build init

---
+++ src/main/java/site/udtk/sentenceapi/SentenceApiApplication.java
@@ -0,0 +1,13 @@
+package site.udtk.sentenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceApiApplication.class, args);
+	}
+
+}
