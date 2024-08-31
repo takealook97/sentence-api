93a99120ffad910ab0bd6d8f374f2c5247a245f2
takealook97 <takealook97@naver.com>
2024-08-31 21:18:51 +0900

    feat: add find method with language & sort on category repository

--- src/main/java/site/udtk/sentenceapi/repository/CategoryRepository.java
+++ src/main/java/site/udtk/sentenceapi/repository/CategoryRepository.java
@@ -1,8 +1,14 @@
 package site.udtk.sentenceapi.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import site.udtk.sentenceapi.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	List<Category> findAllByLanguage(String language);
+
+	Optional<Category> findBySort(String sort);
 }
