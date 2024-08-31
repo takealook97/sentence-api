261dd04681b4256d583ebff9f412440a7103d5b2
takealook97 <takealook97@naver.com>
2024-08-31 21:19:32 +0900

    feat: add "of" factory method on sentence dto

--- src/main/java/site/udtk/sentenceapi/dto/SentenceDto.java
+++ src/main/java/site/udtk/sentenceapi/dto/SentenceDto.java
@@ -1,14 +1,19 @@
 package site.udtk.sentenceapi.dto;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
 import lombok.Builder;
 import lombok.Getter;
+import site.udtk.sentenceapi.domain.Sentence;
 
 @Getter
 @Builder
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public class SentenceDto {
 	private String author;
 	private String content;
+
+	public static SentenceDto of(Sentence sentence) {
+		return SentenceDto.builder()
+			.author(sentence.getAuthor())
+			.content(sentence.getContent())
+			.build();
+	}
 }
