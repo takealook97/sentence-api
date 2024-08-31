2f5fd8465b99e14915a4015a73e5cf2f278657be
takealook97 <takealook97@naver.com>
2024-08-31 22:38:46 +0900

    refactor: set constant to use threshold enum

--- src/main/java/site/udtk/sentenceapi/util/RandomIdsGenerator.java
+++ src/main/java/site/udtk/sentenceapi/util/RandomIdsGenerator.java
@@ -1,5 +1,7 @@
 package site.udtk.sentenceapi.util;
 
+import static site.udtk.sentenceapi.domain.type.Threshold.*;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,13 +10,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RandomIdsGenerator {
-	private static final int SIZE = 4758;
-
 	public static List<Long> generateRandomIds(int count) {
 		Set<Long> randomIds = new HashSet<>();
 
 		while (randomIds.size() < count) {
-			randomIds.add((long)((Math.random() * SIZE) + 1));
+			randomIds.add((long)(Math.random() * DATA_SIZE.getNum()) + 1);
 		}
 		return randomIds.stream().toList();
 	}
