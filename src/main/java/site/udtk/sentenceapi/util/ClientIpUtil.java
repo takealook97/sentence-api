cb787bacc31e0614e2be50766c886e960bba085c
takealook97 <takealook97@naver.com>
2024-09-01 10:03:29 +0900

    feat: add client ip util class for grep remote addr

---
+++ src/main/java/site/udtk/sentenceapi/util/ClientIpUtil.java
@@ -0,0 +1,31 @@
+package site.udtk.sentenceapi.util;
+
+import java.util.Arrays;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class ClientIpUtil {
+	private static final String[] IP_HEADER_CANDIDATES = {
+		"X-Forwarded-For",
+		"Proxy-Client-IP",
+		"WL-Proxy-Client-IP",
+		"HTTP_X_FORWARDED_FOR",
+		"HTTP_X_FORWARDED",
+		"HTTP_X_CLUSTER_CLIENT_IP",
+		"HTTP_CLIENT_IP",
+		"HTTP_FORWARDED_FOR",
+		"HTTP_FORWARDED",
+		"HTTP_VIA",
+	};
+
+	public static String getClientIp(HttpServletRequest request) {
+		return Arrays.stream(IP_HEADER_CANDIDATES)
+			.map(request::getHeader)
+
+			.filter(ipAddress -> ipAddress != null && !ipAddress.isEmpty() && !"unknown".equalsIgnoreCase(ipAddress))
+			.map(ipAddress -> ipAddress.split(",")[0])
+			.findFirst()
+
+			.orElseGet(request::getRemoteAddr);
+	}
+}
