16c8ef189e07a3646b35cb4f77227a9767a857f9
takealook97 <takealook97@naver.com>
2024-09-01 10:03:05 +0900

    chore: add lettuce based proxy manager on redis config

--- src/main/java/site/udtk/sentenceapi/config/RedisConfig.java
+++ src/main/java/site/udtk/sentenceapi/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package site.udtk.sentenceapi.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import site.udtk.sentenceapi.dto.SentenceDto;
 
 @Configuration
@@ -42,5 +53,25 @@ public class RedisConfig {
 		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(SentenceDto.class));
 		redisTemplate.afterPropertiesSet();
 		return redisTemplate;
+	}
+
+	@Bean
+	public ProxyManager<String> lettuceBasedProxyManager() {
+		RedisClient redisClient = redisClient();
+		StatefulRedisConnection<String, byte[]> redisConnection = redisClient
+			.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+		return LettuceBasedProxyManager.builderFor(redisConnection)
+			.withExpirationStrategy(
+				ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(1L)))
+			.build();
+	}
+
+	private RedisClient redisClient() {
+		return RedisClient.create(RedisURI.builder()
+			.withHost(host)
+			.withPort(port)
+			.withPassword(password)
+			.build());
 	}
 }
