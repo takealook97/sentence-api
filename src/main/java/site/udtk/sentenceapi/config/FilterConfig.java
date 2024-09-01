e392b5f29d34b275cf1243ceda66caab9bc8f054
takealook97 <takealook97@naver.com>
2024-09-01 10:02:23 +0900

    feat: add filter config class

---
+++ src/main/java/site/udtk/sentenceapi/config/FilterConfig.java
@@ -0,0 +1,31 @@
+package site.udtk.sentenceapi.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+	private final RateLimitFilter rateLimitFilter;
+	private static final String[] INCLUDE_PATHS = {
+		"/*"
+	};
+
+	@Bean
+	public FilterRegistrationBean<RateLimitFilter> filterBean() {
+
+		FilterRegistrationBean<RateLimitFilter> registrationBean
+			= new FilterRegistrationBean<>();
+
+		registrationBean.setFilter(rateLimitFilter);
+		registrationBean.setOrder(1);
+		registrationBean.setUrlPatterns(Arrays.asList(INCLUDE_PATHS));
+
+		return registrationBean;
+	}
+}
